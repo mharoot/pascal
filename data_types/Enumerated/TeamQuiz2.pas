64d69622378295d53434ffcccefb85e57f1a13fd
mharoot <michaelharootoonyan@yahoo.com>
2017-11-05 23:54:55 -0800

    finished ready to start quiz

--- data_types/Enumerated/TeamQuiz2.pas
+++ data_types/Enumerated/TeamQuiz2.pas
@@ -3,16 +3,10 @@ program EnumeratedTypes;
 uses
   SysUtils;
 type
-  TMonth =(January, February, March, April, May, June, July, August,
-         September, October, November, December);
-  TMonthQuarter1 = January .. March;
-  TSpringMonth = March .. May;
-
-type
-  TGreatLakes = (Erie, Ontario, Huron, Michigan, Superior);
-  TSGreatLakes = Erie .. Ontario;
-  TLGreatLakes = Huron .. Superior;
-
+    TGreatLakes = (Erie, Ontario, Huron, Michigan, Superior);
+    TSGreatLakes = Erie .. Ontario;
+    TLGreatLakes = Huron .. Superior;    
+      
 const
     LakeNames : array [TGreatLakes] of string =
         ('Erie', 'Ontario', 'Huron', 'Michigan', 'Superior');
@@ -20,63 +14,21 @@ const
         (9910, 7340, 23000, 22300, 31700);
     elevation : array [Erie .. Superior] of real =
         (570.38, 244.77, 578.68, 578.68, 600.38);
-
-const
-  AverageLondonRainfall : array [January .. December] of integer =
-                          (61, 36, 50, 42, 45, 46, 46, 44, 43, 73, 45, 59);
-  MonthNames : array [January .. December] of string =
-     ('January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December');
 var
-  Month: TMonth;
-  MonthQuarter1 : TMonthQuarter1;
-  SpringMonth : TSpringMonth;
-  SpringTotal, SummerTotal, AutumnTotal, WinterTotal, MonthNum : integer;
+    GreatLakes: TGreatLakes;
+    SGreatLakes: TSGreatLakes;
+    LGreatLakes: TLGreatLakes;
+
+
+  
+  
 begin
-  //Monthly
-  writeln('The average monthly rainfall (mm) in London is as follows.');
-  for Month := January to December do
+ //GreatLakes
+  writeln('The GreatLakes.');
+  for GreatLakes := Erie to Superior do
     begin
-      writeln(MonthNames[Month] :9, ': ' ,AverageLondonRainfall[Month]);
+      writeln(LakeNames[GreatLakes] :9, ': ' ,area[GreatLakes]);
     end;
   writeln;
-  //Spring
-  SpringTotal := 0;
-  for SpringMonth := low(TSpringMonth) to high(TSpringMonth) do
-    begin
-       SpringTotal := SpringTotal + AverageLondonRainfall[SpringMonth];
-    end;
-  writeln('Total average rainfall (mm) for Spring: ', Springtotal);
-  //Summer
-  SummerTotal := 0;
-  for Month := June to August do
-    begin
-       SummerTotal := SummerTotal + AverageLondonRainfall[Month];
-    end;
-  writeln('Total average rainfall (mm) for Summer: ',SummerTotal);
-  //Autumn
-  AutumnTotal := 0;
-  Month := August;
-  repeat
-     Month := succ(Month);
-     AutumnTotal := AutumnTotal + AverageLondonRainfall[Month];
-  until Month = November;
-  writeln('Total average rainfall (mm) for Autumn: ', AutumnTotal);
-   //Winter
-  WinterTotal := AverageLondonRainfall[December] + AverageLondonRainfall[January] +
-                 AverageLondonRainfall[February];
-  writeln('Total average rainfall (mm) for Winter: ',WinterTotal);
-  writeln;
-  //Selected month
-  write('Which month in the first quarter? Type 1 for Jan, 2 for Feb, 3 for Mar. ' );
-  readln(MonthNum);
-  writeln;
-  case MonthNum of
-    1: MonthQuarter1 := January;
-    2: MonthQuarter1 := February;
-    3: MonthQuarter1 := March;
-  end;
-   writeln('Average rainfall (mm) in London for ',MonthNames[MonthQuarter1],
-                ':  ',AverageLondonRainfall[MonthQuarter1]);
-  readln;
+  
 end.
