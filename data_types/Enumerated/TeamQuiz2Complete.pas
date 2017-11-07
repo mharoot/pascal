4fa18764ab25a02deea7fcab9c88611f40698a13
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2017-11-06 20:00:24 -0800

    finished with quiz 2

---
+++ data_types/Enumerated/TeamQuiz2Complete.pas
@@ -0,0 +1,34 @@
+program EnumeratedTypes;
+
+uses
+  SysUtils;
+  
+
+type GreatLakes = (Erie, Ontario, Huron, Michigan, Superior);
+type LakeProperties = (area, elevation);
+
+
+var
+GL: GreatLakes;
+LP: LakeProperties;
+
+const
+LakeNames : array [GreatLakes] of string =
+        ('Erie', 'Ontario', 'Huron', 'Michigan', 'Superior');
+Lakes:  array [GreatLakes, LakeProperties] of real = 
+( 
+(9910,570.38), (7340, 244.77), (23000, 578.68 ), (22300, 578.68), (31700, 600.38)
+);
+  
+begin
+//  writeln(Lakes [Erie, area]:20:2);
+ // writeln(Lakes [Ontario, area]:20:2);
+ // writeln;
+  
+  for GL := Erie to Superior do
+    begin
+      writeln(LakeNames[GL] :9, ': ' ,Lakes [GL, area]:20:2);
+  end;
+  
+end.
+
